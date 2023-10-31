552ab98653d90e2922b3c90b0697406d15c29ab4
Michael David <lordkay07@gmail.com>
2023-10-31 09:54:45 +0100

    create the html template for the homepage

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,8 @@
 from application import app
+from flask import render_template
 
 @app.route("/")
 @app.route("/index")
 
 def index():
-    return "<h1>Flask, Hello</h1>"
+    return render_template("index.html")
